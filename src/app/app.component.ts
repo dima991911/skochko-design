ff6fe37d21d455b73123cf39d701428f106bb847
pohoretskyi <taras.pogorestskyi@negotiations.com>
2019-09-21 18:57:31 +0300

    create scroll on main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
+
+const logos = [
+  {name: 'New Logo', background: 'red'},
+  {name: 'Old Logo', background: 'blue'},
+  {name: 'Some Logo White', background: 'green'},
+  {name: 'Some Logo Dima', background: 'yellow'},
+  {name: 'Some Logo Vadym', background: 'cyan'},
+  {name: 'Some Logo Olesya', background: 'pink'},
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'skochko-design';
+  canScroll: boolean = true;
+  currentMenuItem: number = 0;
+  logos: object[] = logos;
+
+  @ViewChild('mainContainer', {static: false}) public mainContainer: ElementRef<any>;
+
+  @HostListener('wheel', ['$event'])
+  onScrollMainSection(e) {
+    if (this.canScroll) {
+      this.canScroll = false;
+      if (e.deltaY > 0 && this.logos[this.currentMenuItem + 1]) {
+        document.getElementById('section' + (this.currentMenuItem + 1)).scrollIntoView();
+        this.currentMenuItem += 1;
+      } else if (e.deltaY < 0 && this.logos[this.currentMenuItem - 1]) {
+        document.getElementById('section' + (this.currentMenuItem - 1)).scrollIntoView();
+        this.currentMenuItem -= 1;
+      }
+      setTimeout(() => this.canScroll = true, 1000);
+    }
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(e) {
+    console.log(e);
+  }
 }
