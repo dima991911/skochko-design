a27ffacc9b703bc05a07ada7093bc8ace7d719dc
pohoretskyi <taras.pogorestskyi@negotiations.com>
2019-09-21 19:11:16 +0300

    scroll on main page for mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,12 @@ const logos = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  logos: object[] = logos;
+
   canScroll: boolean = true;
   currentMenuItem: number = 0;
-  logos: object[] = logos;
+
+  touchStartYoffset: number;
 
   @ViewChild('mainContainer', {static: false}) public mainContainer: ElementRef<any>;
 
@@ -26,18 +29,35 @@ export class AppComponent {
     if (this.canScroll) {
       this.canScroll = false;
       if (e.deltaY > 0 && this.logos[this.currentMenuItem + 1]) {
-        document.getElementById('section' + (this.currentMenuItem + 1)).scrollIntoView();
-        this.currentMenuItem += 1;
+        // document.getElementById('section' + (this.currentMenuItem + 1)).scrollIntoView();
+        // this.currentMenuItem += 1;
+        this.nextMenuItem(this.currentMenuItem + 1);
       } else if (e.deltaY < 0 && this.logos[this.currentMenuItem - 1]) {
-        document.getElementById('section' + (this.currentMenuItem - 1)).scrollIntoView();
-        this.currentMenuItem -= 1;
+        // document.getElementById('section' + (this.currentMenuItem - 1)).scrollIntoView();
+        // this.currentMenuItem -= 1;
+        this.nextMenuItem(this.currentMenuItem - 1);
       }
       setTimeout(() => this.canScroll = true, 1000);
     }
   }
 
-  @HostListener('touchmove', ['$event'])
-  onTouchMove(e) {
-    console.log(e);
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(e) {
+    this.touchStartYoffset = e.changedTouches[0].screenY;
   }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(e) {
+    if (e.changedTouches[0].screenY < this.touchStartYoffset && this.logos[this.currentMenuItem + 1]) {
+      this.nextMenuItem(this.currentMenuItem + 1);
+    } else if (e.changedTouches[0].screenY > this.touchStartYoffset && this.logos[this.currentMenuItem - 1]) {
+      this.nextMenuItem(this.currentMenuItem - 1);
+    }
+  }
+
+  nextMenuItem(item: number) {
+    document.getElementById('section' + (item)).scrollIntoView();
+    this.currentMenuItem = item;
+  }
+
 }
