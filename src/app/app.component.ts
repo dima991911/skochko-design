e59cf4344a04b200014d5bcb111b2a175742985e
pohoretskyi <taras.pogorestskyi@negotiations.com>
2019-09-22 19:09:01 +0300

    create menu container with animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('toggleMenu', [
+      state('showMenu', style({
+        transform: 'translateX(0)',
+        zIndex: 9
+      })),
+      state('hideMenu', style({
+        transform: 'translateX(100%)',
+        zIndex: 0
+      })),
+      transition('showMenu=>hideMenu', animate('350ms ease-out')),
+      transition('hideMenu=>showMenu', animate('350ms ease-out'))
+    ]),
+  ]
 })
 export class AppComponent {
+  isMenuVisible: string = 'hideMenu';
 
+  onToggleMenu() {
+    this.isMenuVisible = this.isMenuVisible === 'hideMenu' ? 'showMenu' : 'hideMenu';
+  }
 }
